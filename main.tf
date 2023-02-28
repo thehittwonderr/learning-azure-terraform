d943dfde92ae7075338e0ceb626659da9849ee7a
JSantos05 <JSantos0005@gmail.com>
2023-02-28 14:36:36 -0500

    creating vnet

--- main.tf
+++ main.tf
@@ -6,3 +6,24 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = random_pet.rg_name.id
 }
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
