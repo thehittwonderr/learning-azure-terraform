016cda758715aed6ba3d791c66ebae96edfa727d
thehittwonderr <Thehittwonderr@gmail.com>
2023-03-06 14:27:54 -0500

    removing storage account

--- vms.tf
+++ vms.tf
@@ -27,23 +27,23 @@
 #}
 #
 ## Generate random text for a unique storage account name
-resource "random_id" "random_id" {
-  keepers = {
-    # Generate a new ID only when a new resource group is defined
-    resource_group = azurerm_resource_group.rg.name
-  }
+#resource "random_id" "random_id" {
+#  keepers = {
+# Generate a new ID only when a new resource group is defined
+#    resource_group = azurerm_resource_group.rg.name
+#  }
 
-  byte_length = 8
-}
+#  byte_length = 8
+#}
 #
 ## Create storage account for boot diagnostics
-resource "azurerm_storage_account" "my_storage_account" {
-  name                     = "diag${random_id.random_id.hex}"
-  location                 = azurerm_resource_group.rg.location
-  resource_group_name      = azurerm_resource_group.rg.name
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
+#resource "azurerm_storage_account" "my_storage_account" {
+#  name                     = "diag${random_id.random_id.hex}"
+#  location                 = azurerm_resource_group.rg.location
+#  resource_group_name      = azurerm_resource_group.rg.name
+#  account_tier             = "Standard"
+#  account_replication_type = "LRS"
+#}
 #
 ## Create (and display) an SSH key
 #resource "tls_private_key" "example_ssh" {
