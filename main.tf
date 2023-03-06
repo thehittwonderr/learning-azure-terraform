9775275a24298fd1c2028ae92c126b11126d3b5b
thehittwonderr <Thehittwonderr@gmail.com>
2023-03-06 09:21:32 -0500

    add ssh security rule to nsg

--- main.tf
+++ main.tf
@@ -11,6 +11,18 @@ resource "azurerm_network_security_group" "nsg1" {
   name                = "example-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-SSH-In"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_virtual_network" "example" {
