36758b1feb92e9cd340a22b8750ba0bcd3862a6d
thehittwonderr <Thehittwonderr@gmail.com>
2023-03-08 13:01:12 -0500

    delete vms and lbs

--- lb.tf
+++ lb.tf
@@ -1,15 +1,15 @@
-resource "azurerm_lb" "example" {
-  name                = "TestLoadBalancer"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  frontend_ip_configuration {
-    name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.my_terraform_public_ip.id
-  }
-}
-
-resource "azurerm_lb_backend_address_pool" "example" {
-  loadbalancer_id = azurerm_lb.example.id
-  name            = "BackEndAddressPool"
-}
+#resource "azurerm_lb" "example" {
+#  name                = "TestLoadBalancer"
+#  location            = azurerm_resource_group.rg.location
+#  resource_group_name = azurerm_resource_group.rg.name
+#
+#  frontend_ip_configuration {
+#    name                 = "PublicIPAddress"
+#    public_ip_address_id = azurerm_public_ip.my_terraform_public_ip.id
+#  }
+#}
+#
+#resource "azurerm_lb_backend_address_pool" "example" {
+#  loadbalancer_id = azurerm_lb.example.id
+#  name            = "BackEndAddressPool"
+#}
