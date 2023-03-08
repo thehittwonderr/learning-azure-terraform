0bfdc6735cf525fd7c50efc6f882ae0ea33bbefa
thehittwonderr <Thehittwonderr@gmail.com>
2023-03-08 11:43:04 -0500

    create vm and pip

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,11 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
-#output "public_ip_address" {
-#  value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
-#}
-#
-#output "tls_private_key" {
-#  value     = tls_private_key.example_ssh.private_key_pem
-#  sensitive = true
-#}
+output "public_ip_address" {
+  value = azurerm_linux_virtual_machine.example.id
+}
+
+output "tls_private_key" {
+  value     = tls_private_key.example_ssh.private_key_pem
+  sensitive = true
+}
