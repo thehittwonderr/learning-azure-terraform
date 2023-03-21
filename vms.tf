a117ee056b62e866bc8057bee20c8bda2504d907
thehittwonderr <Thehittwonderr@gmail.com>
2023-03-21 16:54:12 -0400

    updating pip sku

--- vms.tf
+++ vms.tf
@@ -10,6 +10,7 @@ resource "azurerm_public_ip" "my_terraform_public_ip" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_network_interface" "example" {
