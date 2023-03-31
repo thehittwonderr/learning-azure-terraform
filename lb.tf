f9f4cc7b3748472d0049cbf845ade3c15044b65b
thehittwonderr <thehittwonderr@gmail.com>
2023-03-31 10:43:15 -0400

    restart vms change outputs

--- lb.tf
+++ lb.tf
@@ -1,34 +1,34 @@
-#resource "azurerm_lb" "example" {
-#  name                = "TestLoadBalancer"
-#  location            = azurerm_resource_group.rg.location
-#  resource_group_name = azurerm_resource_group.rg.name
-#  sku                 = "Standard"
-#
-#  frontend_ip_configuration {
-#    name                 = "PublicIPAddress"
-#    public_ip_address_id = azurerm_public_ip.my_terraform_public_ip.id
-#  }
-#}
-#
-#resource "azurerm_lb_backend_address_pool" "example" {
-#  loadbalancer_id = azurerm_lb.example.id
-#  name            = "BackEndAddressPool"
-#}
-#
-#resource "azurerm_lb_backend_address_pool_address" "example1" {
-#  name                    = "example1"
-#  backend_address_pool_id = azurerm_lb_backend_address_pool.example.id
-#  virtual_network_id      = azurerm_virtual_network.example.id
-#  ip_address              = "10.0.3.4"
-#  depends_on              = [azurerm_linux_virtual_machine.example]
-#}
-#
-#resource "azurerm_lb_backend_address_pool_address" "example2" {
-#  name                    = "example2"
-#  backend_address_pool_id = azurerm_lb_backend_address_pool.example.id
-#  virtual_network_id      = azurerm_virtual_network.example.id
-#  ip_address              = "10.0.3.5"
-#  depends_on              = [azurerm_linux_virtual_machine.example]
-#}
-#
-#
+resource "azurerm_lb" "example" {
+  name                = "TestLoadBalancer"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.my_terraform_public_ip.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "example" {
+  loadbalancer_id = azurerm_lb.example.id
+  name            = "BackEndAddressPool"
+}
+
+resource "azurerm_lb_backend_address_pool_address" "example1" {
+  name                    = "example1"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.example.id
+  virtual_network_id      = azurerm_virtual_network.example.id
+  ip_address              = "10.0.3.4"
+  depends_on              = [azurerm_linux_virtual_machine.example]
+}
+
+resource "azurerm_lb_backend_address_pool_address" "example2" {
+  name                    = "example2"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.example.id
+  virtual_network_id      = azurerm_virtual_network.example.id
+  ip_address              = "10.0.3.5"
+  depends_on              = [azurerm_linux_virtual_machine.example]
+}
+
+
--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,19 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
-#output "public_ip_address" {
-#  value = azurerm_linux_virtual_machine.example.id
-#}
-#
-#output "tls_private_key" {
-#  value     = tls_private_key.example_ssh.private_key_pem
-#  sensitive = true
-#}
+output "private_ip_address" {
+  value = tomap({
+    for name, vm in azurerm_network_interface.example : name => vm.private_ip_address
+  })
+}
+#  value = azurerm_network_interface.example[count.index].private_ip_address
+
+
+output "tls_private_key" {
+  value     = tls_private_key.example_ssh.private_key_pem
+  sensitive = true
+}
+
+output "public_ip_lb" {
+  value = azurerm_public_ip.my_terraform_public_ip.id
+}
