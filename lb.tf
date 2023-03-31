eb2ec3c0db7e6f2b2d28fd86c55d1839aa9cf36f
thehittwonderr <126593746+thehittwonderr@users.noreply.github.com>
2023-03-31 13:37:11 -0400

    shutdown (#29)

--- lb.tf
+++ lb.tf
@@ -1,44 +1,44 @@
-resource "azurerm_lb" "example" {
-  name                = "TestLoadBalancer"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "Standard"
-
-  frontend_ip_configuration {
-    name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.my_terraform_public_ip.id
-  }
-}
-
-resource "azurerm_lb_backend_address_pool" "example" {
-  loadbalancer_id = azurerm_lb.example.id
-  name            = "BackEndAddressPool"
-}
-
-resource "azurerm_lb_backend_address_pool_address" "example1" {
-  name                    = "example1"
-  backend_address_pool_id = azurerm_lb_backend_address_pool.example.id
-  virtual_network_id      = azurerm_virtual_network.example.id
-  ip_address              = "10.0.3.4"
-  depends_on              = [azurerm_linux_virtual_machine.example]
-}
-
-resource "azurerm_lb_backend_address_pool_address" "example2" {
-  name                    = "example2"
-  backend_address_pool_id = azurerm_lb_backend_address_pool.example.id
-  virtual_network_id      = azurerm_virtual_network.example.id
-  ip_address              = "10.0.3.5"
-  depends_on              = [azurerm_linux_virtual_machine.example]
-}
-
-resource "azurerm_lb_rule" "example" {
-  resource_group_name            = azurerm_resource_group.rg.name
-  loadbalancer_id                = azurerm_lb.example.id
-  name                           = "AllowSSH"
-  protocol                       = "Tcp"
-  frontend_port                  = 22
-  backend_port                   = 22
-  frontend_ip_configuration_name = "PublicIPAddress"
-  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.example.id]
-}
-
+#resource "azurerm_lb" "example" {
+#  name                = "TestLoadBalancer"
+#  location            = azurerm_resource_group.rg.location
+#  resource_group_name = azurerm_resource_group.rg.name
+#  sku                 = "Standard"
+#
+#  frontend_ip_configuration {
+#    name                 = "PublicIPAddress"
+#    public_ip_address_id = azurerm_public_ip.my_terraform_public_ip.id
+#  }
+#}
+#
+#resource "azurerm_lb_backend_address_pool" "example" {
+#  loadbalancer_id = azurerm_lb.example.id
+#  name            = "BackEndAddressPool"
+#}
+#
+#resource "azurerm_lb_backend_address_pool_address" "example1" {
+#  name                    = "example1"
+#  backend_address_pool_id = azurerm_lb_backend_address_pool.example.id
+#  virtual_network_id      = azurerm_virtual_network.example.id
+#  ip_address              = "10.0.3.4"
+#  depends_on              = [azurerm_linux_virtual_machine.example]
+#}
+#
+#resource "azurerm_lb_backend_address_pool_address" "example2" {
+#  name                    = "example2"
+#  backend_address_pool_id = azurerm_lb_backend_address_pool.example.id
+#  virtual_network_id      = azurerm_virtual_network.example.id
+#  ip_address              = "10.0.3.5"
+#  depends_on              = [azurerm_linux_virtual_machine.example]
+#}
+#
+#resource "azurerm_lb_rule" "example" {
+#  resource_group_name            = azurerm_resource_group.rg.name
+#  loadbalancer_id                = azurerm_lb.example.id
+#  name                           = "AllowSSH"
+#  protocol                       = "Tcp"
+#  frontend_port                  = 22
+#  backend_port                   = 22
+#  frontend_ip_configuration_name = "PublicIPAddress"
+#  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.example.id]
+#}
+#
