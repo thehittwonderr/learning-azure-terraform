6147af64e3a278931f188b3c2b6953afc4ff9aeb
thehittwonderr <thehittwonderr@gmail.com>
2023-06-21 12:40:51 -0400

    build aks

--- containers/aks.tf
+++ containers/aks.tf
@@ -1,18 +1,18 @@
-#resource "azurerm_kubernetes_cluster" "example" {
-#  name                = "example-aks1"
-#  location            = azurerm_resource_group.rg.location
-#  resource_group_name = azurerm_resource_group.rg.name
-#  dns_prefix          = "exampleaks1"
-#
-#  default_node_pool {
-#    name       = "default"
-#    node_count = 1
-#    vm_size    = "Standard_D2_v2"
-#  }
-#
-#  identity {
-#    type = "SystemAssigned"
-#  }
-#
-#}
-#
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "example-aks1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+}
+
--- containers/main.tf
+++ containers/main.tf
@@ -2,3 +2,18 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = "EUS-containers"
 }
+
+#module "new_network" {
+#  source = "/Users/javier/terraform/modules"
+#
+#  resource_group_location = "eastus2"
+#  resource_group_name     = "test-rg"
+#  nsg_name                = "test-nsg"
+#  vnet_name               = "testing"
+#  cidr                    = ["192.168.0.0/16"]
+#  sn01                    = "192.168.1.0/24"
+#  sn02                    = "192.168.2.0/24"
+#  sn03                    = "192.168.3.0/24"
+#  env-tag                 = "test"
+#
+#}
